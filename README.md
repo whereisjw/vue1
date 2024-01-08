13278bae886820be62a2ec0d9664f3de4f9733f9
whereisjw <j9399977@gmail.com>
2024-01-08 14:56:06 +0900

    vue 반복문학습

--- README.md
+++ README.md
@@ -1,1 +1,24 @@
 # vue
+
+- node,vscode,yarn 설치
+- npm install -g @vue/cli
+- vue create 프로젝트명
+- npm run serve
+
+# 반복문 데이터바인딩
+
+```
+  <a v-for="반복문이름 in 반복횟수" :key="키값">내용</a>
+//기본구성
+
+<a v-for="작명 in menu" :key="작명">{{ 작명 }}</a>
+     menu:['홈','상품','상세'],
+
+
+  <div v-for="(v,i) in products" :key="i">
+    <h4>{{ v }}</h4>
+    <h3>50만원</h3>
+  </div>
+
+
+```
--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,50 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+
+  <div class="menu">
+    <a v-for="(value,index) in menu" :key="index">{{ value }}</a>
+  </div>
+<!--   <img alt="Vue logo" src="./assets/logo.png" />   -->
+
+  <div v-for="(v,i) in products" :key="i">
+    <h4>{{ v }}</h4>
+    <h3>50만원</h3>
+  </div>
+ 
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
 export default {
-  name: 'App',
-  components: {
-    HelloWorld
-  }
-}
+  name: "App",
+  data(){
+    return{
+      price1 : 60,
+      price2 : 70,
+      menu:['홈','상품','상세'],
+    products:['역삼동원룸','천호동원룸','마포구원룸']
+    }
+  },
+  components: {},
+};
 </script>
 
 <style>
 #app {
+  
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
-  margin-top: 60px;
+ 
+}
+.menu{
+  background-color: darkslateblue;
+  padding: 15px;
+  border-radius: 5px;
+
+}
+.menu a {
+  color: white;
+  padding: 10px;
 }
 </style>
