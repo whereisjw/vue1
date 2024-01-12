69c0b344d594900a3b08163b5471f57c6c5b1aac
whereisjw <j9399977@gmail.com>
2024-01-12 10:08:10 +0900

    컴포넌트 만들기

--- src/App.vue
+++ src/App.vue
@@ -1,47 +1,97 @@
 <template>
+  <ModalItem :원룸들="원룸들" :누른거="누른거" :모달창열렸니="모달창열렸니" />
 
   <div class="menu">
-    <a v-for="(value,index) in menu" :key="index">{{ value }}</a>
+    <a v-for="(value, index) in menu" :key="index">{{ value }}</a>
   </div>
-<!--   <img alt="Vue logo" src="./assets/logo.png" />   -->
 
-  <div v-for="(v,i) in products" :key="i">
-    <h4>{{ v }}</h4>
-    <h3>50만원</h3>
+  <div class="discount">
+    <h4>지금 결제하면 20% 할인</h4>
   </div>
- 
+
+  <div v-for="(v, i) in 원룸들" :key="i">
+    <img class="room_img" :src="v.image" alt="" />
+    <h4
+      v-on:click="
+        모달창열렸니 = true;
+        누른거 = i;
+      ">
+      {{ v.title }}
+    </h4>
+    <p>{{ v.price }}원</p>
+    <button v-on:click="increase(0)">허위매물신고버튼</button>
+    <span>신고수 : {{ 신고수[0] }}</span>
+  </div>
 </template>
-
 <script>
+import ModalItem from "./Modal.vue";
+import roomData from "./oneroom.js";
 export default {
   name: "App",
-  data(){
-    return{
-      price1 : 60,
-      price2 : 70,
-      menu:['홈','상품','상세'],
-    products:['역삼동원룸','천호동원룸','마포구원룸']
-    }
+  data() {
+    return {
+      누른거: 0,
+      원룸들: roomData,
+      모달창열렸니: false,
+      신고수: [0, 0, 0],
+      price1: 60,
+      price2: 70,
+
+      menu: ["홈", "상품", "상세"],
+      products: ["역삼동원룸", "천호동원룸", "마포구원룸"],
+    };
   },
-  components: {},
+  methods: {
+    increase(a) {
+      this.신고수[a] = this.신고수[a] + 1;
+    },
+  },
+  components: { ModalItem },
 };
 </script>
 
 <style>
+body {
+  margin: 0;
+}
+div {
+  box-sizing: border-box;
+}
+.discount {
+  background: gray;
+  padding: 20px;
+  margin: 10px;
+  box-sizing: border-box;
+  border: 1px solid black;
+}
+.black-bg {
+  width: 100%;
+  height: 100%;
+  background: rgba(0, 0, 0, 0.5);
+  position: fixed;
+  padding: 20px;
+}
+.white-bg {
+  width: 100%;
+  background: white;
+  border-radius: 8px;
+  padding: 20px;
+}
+.room_img {
+  margin-top: 40px;
+  width: 100%;
+}
 #app {
-  
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
- 
 }
-.menu{
+.menu {
   background-color: darkslateblue;
   padding: 15px;
   border-radius: 5px;
-
 }
 .menu a {
   color: white;
---
+++ src/Modal.vue
@@ -0,0 +1,19 @@
+<template>
+  <div class="black-bg" v-if="모달창열렸니">
+    <div class="white-bg">
+      <h4>{{ 원룸들[누른거].title }}</h4>
+      <h3>{{ 원룸들[누른거].content }}</h3>
+      <h3>{{ 원룸들[누른거].price }}</h3>
+
+      <!--  <button v-on:click="모달창열렸니 = false">닫기</button> -->
+    </div>
+  </div>
+  12
+</template>
+<script>
+export default {
+  name: "ModalItem",
+  props: { 원룸들: Array, 누른거: Number, 모달창열렸니: Boolean },
+};
+</script>
+<style></style>
